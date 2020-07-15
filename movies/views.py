86ec124b31e8b8462b86ab9ca37115e65ab5bb85
alaskast0rm <montanaof300@mail.ru>
2020-07-15 03:44:31 +0300

    SEVENTH COMMIT: The block of a last added movies and was been added. The pages of actors and directors were added.

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie
+from .models import Movie, Category, Actor
 from .forms import ReviewForms
 
 
@@ -10,10 +10,20 @@ class MoviesView(ListView):
     model = Movie
     queryset = model.objects.all()
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
 
 class MovieDetailView(DetailView):
     model = Movie
     slug_field = "url"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["categories"] = Category.objects.all()
+        return context
 
 
 class AddReview(View):
@@ -27,3 +37,10 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(DetailView):
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = "name"
+
