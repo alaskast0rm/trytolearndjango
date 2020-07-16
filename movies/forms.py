ed80b88b1b8cd174f50b283573d5b98d53296453
alaskast0rm <montanaof300@mail.ru>
2020-07-17 02:14:33 +0300

    Add rating to the movie.

--- movies/forms.py
+++ movies/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
-from .models import Reviews
+from .models import Reviews, RatingStar, Rating
 
 
 class ReviewForms(forms.ModelForm):
     class Meta:
         model = Reviews
         fields = ("name", "email", "text")
+
+
+class RatingForm(forms.ModelForm):
+    star = forms.ModelChoiceField(
+        queryset=RatingStar.objects.all(),
+        widget=forms.RadioSelect(),
+        empty_label=None
+    )
+
+    class Meta:
+        model = Rating
+        fields = ("star",)
